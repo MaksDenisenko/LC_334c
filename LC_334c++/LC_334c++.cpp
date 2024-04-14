fd6bdbdf638cf17ebfab380fd2671e5d50a757fe
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-04-14 17:12:01 +0300

    Добавьте файлы проекта.

---
+++ LC_334c++/LC_334c++.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool increasingTriplet(vector<int>& nums) {
+    int j = 0, k = 0;
+    for (int i = 0; i < nums.size() - 2; i++)
+    {
+        j = i + 1;
+        while (j != nums.size() - 1)
+        {
+            if (nums[i] < nums[j])
+            {
+                k = j + 1;
+                while (k != nums.size())
+                {
+                    if (nums[j] < nums[k])
+                        return true;
+                    k++;
+                }
+            }
+            j++;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> a = { 10, 5, 1, 4, 2, 3, 2, 1 };
+    increasingTriplet(a);
+}
+
